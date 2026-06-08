0affd9bdc5602a14c1fb4aba9e9b7975f463c263
Javili <jjjavier557@gmail.com>
2026-06-07 22:09:10 -0600

    Arreglo de cors en backend para el servicio https

---
+++ backend/src/main/java/com/projectf1/ruano/alm09/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.projectf1.ruano.alm09.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "http://localhost",
+                                "http://34.70.121.9",
+                                "https://34.70.121.9",
+                                "http://proyectosiso2.ddns.net",
+                                "https://proyectosiso2.ddns.net"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
